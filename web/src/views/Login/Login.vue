cb121a95975f144f055072dcde665219b4cbe78e
Fáiza Schoeninger <faiza.schoeninger@universo.univates.br>
2021-03-24 19:41:28 -0300

    Feat: Add login form.

--- web/src/views/Login/Login.vue
+++ web/src/views/Login/Login.vue
@@ -3,13 +3,59 @@
     <div class="page-title">
       Login
     </div>
+    <div>
+      <form id="login" @submit="login">
+        <section>
+          <fieldset>
+            <div>
+              <label>Usuário ou E-mail:</label>
+              <input type="text" name="username" v-model="username" />
+            </div>
+            <div>
+              <label>Senha:</label>
+              <input type="password" name="passowrd" v-model="password" />
+            </div>
+            <div>
+              <button type="submit">Entrar</button>
+            </div>
+          </fieldset>
+        </section>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 
+import api from '../../services/api';
+
 export default {
   name: 'Login',
+
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+
+  methods: {
+    login(event) {
+      event.preventDefault();
+
+      if (this.username !== '' && this.password !== '') {
+        api.post('/auth/login', {
+          email: this.username,
+          password: this.password,
+        }).then((response) => {
+          if (response.data.access_token) {
+            alert('Autenticação realizada.');
+            window.location.href = '/';
+          }
+        })
+      }
+    },
+  }
 }
 </script>
 
