405a838936c2d88e3795b67795569bd11edb6850
Fáiza Schoeninger <faiza.schoeninger@universo.univates.br>
2021-06-28 14:17:13 -0300

    Create Alert component.

---
+++ web/src/components/Alert/Alert.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition name="fade">
+    <div :class="`${type} alert-message`" v-if="show">
+      {{message}}
+      <i class="icon fas fa-info-circle" />
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: 'Alert',
+
+  props: ['show', 'message', 'type']
+}
+</script>
+
+<style lang="scss">
+  @import "./styles.scss";
+</style>
