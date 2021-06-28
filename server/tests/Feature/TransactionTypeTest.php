f32857c90a904b7e1d1973e5c4fefa91c957ed3a
Fáiza Schoeninger <faiza.schoeninger@universo.univates.br>
2021-06-28 00:03:58 -0300

    Create test to testing the create of transaction type.

--- server/tests/Feature/TransactionTypeTest.php
+++ server/tests/Feature/TransactionTypeTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Enums\OperationType;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -9,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 
 use App\User;
 
+use Faker\Factory as Faker;
 use Tests\TestCase;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
@@ -32,5 +34,44 @@ class TransactionTypeTest extends TestCase
             ->assertJsonStructure([
                 'success', 'message', 'transaction_types'
             ]);
+    }
+
+    /**
+     * Create an transaction type.
+     *
+     * @return void
+     */
+    public function testCreateTransactionType()
+    {
+        $user    = User::where('email', config('test.api.email'))->first();
+        $token   = JWTAuth::fromUser($user);
+
+        $response = $this->createTransactionType($token);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'success', 'message', 'transaction_type'
+            ]);
+    }
+
+    /**
+     * Create transaction type
+     *
+     * @return Response
+     */
+    private function createTransactionType(String $token)
+    {
+        $baseUrl = config('app.url') . '/api/transaction-types?token=' . $token;
+
+        $faker = Faker::create();
+
+        $response = $this->json('POST', $baseUrl . '/', [
+            'code'      => $faker->numberBetween(0, 10),
+            'name'      => $faker->name,
+            'operation' => $faker->numberBetween(0, 1),
+        ]);
+
+        return $response;
     }
 }
