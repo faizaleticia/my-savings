ef86b91010febb4f20fa9ce159f8153267773244
Fáiza Schoeninger <faiza.schoeninger@universo.univates.br>
2021-06-28 12:30:07 -0300

    Add logout option in fronend.

--- web/src/components/Header/Header.vue
+++ web/src/components/Header/Header.vue
@@ -6,18 +6,50 @@
         </a>
       </div>
       <nav class="menu">
-				<ul class="">
+				<ul v-if="!isAuthenticated">
 					<li><a href="/register">REGISTRAR</a></li>
           <li><a href="/login">ENTRAR</a></li>
 				</ul>
+        <ul v-else>
+          <li><a @click="logout">SAIR</a></li>
+        </ul>
 			</nav>
     </header>
 </template>
 
 <script>
 
+import api from '../../services/api';
+
 export default {
   name: 'Header',
+
+  computed: {
+    isAuthenticated () {
+      const token = localStorage.getItem('user-token');
+      return token && token != '';
+    },
+  },
+
+  methods: {
+    logout () {
+     api.post('/auth/logout', {
+        headers: {
+          'Authorization': `Bearer ${localStorage.getItem('user-token')}`
+        }
+      }).then((response) => {
+        if (response.data.message == 'Successfully logged out') {
+          localStorage.removeItem('user-token');
+          window.location.href = '/';
+        }
+      }).catch((error) => {
+        if (error.response.data.message == 'Unauthenticated.') {
+          localStorage.removeItem('user-token');
+          window.location.href = '/';
+        }
+      });
+    },
+  },
 }
 </script>
 
