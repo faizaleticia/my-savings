060fe1740c5269b708e6fc8cf9936d1462e0bc79
Fáiza Schoeninger <faiza.schoeninger@universo.univates.br>
2021-06-28 14:51:47 -0300

    Create Transaction table.

---
+++ server/database/migrations/2021_06_28_174809_create_transaction_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('account_id');
+            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->decimal('value', 20, 4);
+            $table->unsignedBigInteger('transaction_type_id');
+            $table->foreign('transaction_type_id')->references('id')->on('transaction_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
